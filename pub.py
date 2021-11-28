83d0bf94eb19301b24c90d214b712e46fd457713
rosroslab <95176346+rosroslab@users.noreply.github.com>
2021-11-28 19:58:11 +0530

    Add files via upload

---
+++ pub.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import rospy
+from std_msgs.msg import Float64MultiArray
+
+if __name__ == '__main__':
+
+    rospy.init_node("Give_Commands")
+    x = rospy.Publisher("/trpy", Float64MultiArray, queue_size=10)
+
+    while(not rospy.is_shutdown()):
+
+        msg = Float64MultiArray()
+        msg.data = [60.0, 0, 0, 0]
+
+        x.publish(msg)
+
+    
